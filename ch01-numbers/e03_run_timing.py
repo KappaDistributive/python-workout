21c85f632d10eea7b3e1418e97a8cccfb64fc1f2
Stefan Mesken <kappadistributive@gmail.com>
2022-05-25 20:42:06 +0200

    solve e03

--- ch01-numbers/e03_run_timing.py
+++ ch01-numbers/e03_run_timing.py
@@ -1,24 +1,16 @@
-#!/usr/bin/env python3
-"""Solution to chapter 1, exercise 3: run_timing"""
-
-
 def run_timing():
-    """Asks the user repeatedly for numeric input.
-Prints the average time and number of runs.
-"""
-
-    number_of_runs = 0
     total_time = 0
+    num_entries = 0
 
     while True:
-        one_run = input('Enter 10 km run time: ')
-
-        if not one_run:
+        user_input = input("Enter 10 km run time: ")
+        if not user_input:
             break
+        total_time += float(user_input)
+        num_entries += 1
 
-        number_of_runs += 1
-        total_time += float(one_run)
+    average_time = total_time / num_entries
+    print(f"Average of {average_time}, over {num_entries} runs")
 
-    average_time = total_time / number_of_runs
-
-    print(f'Average of {average_time}, over {number_of_runs} runs')
+if __name__ == "__main__":
+    run_timing()
