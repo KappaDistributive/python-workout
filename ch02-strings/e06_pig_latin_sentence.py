abe6713c9be9e04fb23f25b9357a01cc0ce4ed6a
Stefan Mesken <kappadistributive@gmail.com>
2022-05-27 16:52:05 +0200

    solve e06

--- ch02-strings/e06_pig_latin_sentence.py
+++ ch02-strings/e06_pig_latin_sentence.py
@@ -1,17 +1,10 @@
-#!/usr/bin/env python3
-"""Solution to chapter 2, exercise 6: pl_sentence"""
+def pig_latin(word: str) -> str:
+    if not word:
+        return ""
+    if word[0] in "aeiou":
+        return word + "way"
+    return word[1:] + word[0] + "ay"
 
+def pl_sentence(text: str) -> str:
+    return " ".join(pig_latin(word) for word in text.split(" "))
 
-def pl_sentence(sentence):
-    """Get a sentence from the user, containing
-lowercase, unpuncutated words. Return the
-sentence, translated into Pig Latin.
-"""
-    output = []
-    for word in sentence.split():
-        if word[0] in 'aeiou':
-            output.append(f'{word}way')
-        else:
-            output.append(f'{word[1:]}{word[0]}ay')
-
-    return ' '.join(output)
