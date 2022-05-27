2a2daf60ee5e415ea63a85b3c5620ae2630eceee
Stefan Mesken <kappadistributive@gmail.com>
2022-05-27 16:46:53 +0200

    solve e05

--- ch02-strings/e05_pig_latin.py
+++ ch02-strings/e05_pig_latin.py
@@ -1,13 +1,6 @@
-#!/usr/bin/env python3
-"""Solution to chapter 2, exercise 5: pig_latin"""
-
-
-def pig_latin(word):
-    """Translates a word into Pig Latin.
-The "word" parameter is assumed to be an
-English word, returned as a string.
-"""
-    if word[0] in 'aeiou':
-        return f'{word}way'
-
-    return f'{word[1:]}{word[0]}ay'
+def pig_latin(word: str) -> str:
+    if not word:
+        return ""
+    if word[0] in "aeiou":
+        return word + "way"
+    return word[1:] + word[0] + "ay"
