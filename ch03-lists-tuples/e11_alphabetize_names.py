eaf038762981de2872c692f1e744c896ec5c73b0
Stefan Mesken <kappadistributive@gmail.com>
2022-05-29 21:34:26 +0200

    solve e11

--- ch03-lists-tuples/e11_alphabetize_names.py
+++ ch03-lists-tuples/e11_alphabetize_names.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python3
-"""Solution to chapter 3, exercise 11: alphabetize_names"""
-
-import operator
-
-
 PEOPLE = [{'first': 'Reuven', 'last': 'Lerner',
            'email': 'reuven@lerner.co.il'},
           {'first': 'Donald', 'last': 'Trump',
@@ -13,13 +7,5 @@ PEOPLE = [{'first': 'Reuven', 'last': 'Lerner',
           ]
 
 
-def alphabetize_names(list_of_dicts):
-    """Take a list of dicts describing people,
-each with first/last/email as keys.
-
-Return a new list of dicts,
-sorted first by last name and then by first name.
-
-If passed an empty list, then return an empty list.
-"""
-    return sorted(list_of_dicts, key=operator.itemgetter('last', 'first'))
+def alphabetize_names(people):
+    return sorted(people, key=lambda entry: (entry["last"], entry["first"]))
